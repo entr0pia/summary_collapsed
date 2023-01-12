e8518dfd1ff56969b59189b9d2508b9950e544fc
entr0pia <30486766+entr0pia@users.noreply.github.com>
2023-01-12 13:09:56 +0800

    v1

--- customize.sh
+++ customize.sh
@@ -1,3 +1,7 @@
 #模块内容全部进magisk系统=0
 #更多自定义=1
 SKIPUNZIP=0
+
+if [ "$(getprop ro.product.vendor.brand)" != "Onyx" ] || [ "$(getprop ro.product.vendor.device)" != "Nova2" ] || [ "$(getprop ro.build.version.release)" != "9" ]; then
+  abort "仅保障适用于【文石 Nova2】, Android 9"
+fi
